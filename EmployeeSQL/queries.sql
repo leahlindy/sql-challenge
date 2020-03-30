3b3acd82e97143bcce696a0345de30846128faf7
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-03-30 14:30:55 -0400

    queries

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,14 @@
+-- Database: Employees_db
+
+-- DROP DATABASE "Employees_db";
+
+CREATE DATABASE "Employees_db"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+-- Queries 
