c6896945f97625ce7827ce25c0a629ef7dd90f80
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-03-30 14:24:25 -0400

    Edit ERD

--- EmployeeSQL/schemata.sql
+++ EmployeeSQL/schemata.sql
@@ -19,8 +19,8 @@ CREATE TABLE "dept_emp" (
 );
 
 CREATE TABLE "dept_manager" (
-    "emp_no" INTEGER   NOT NULL,
     "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
     "from_date" VARCHAR   NOT NULL,
     "to_date" VARCHAR   NOT NULL
 );
@@ -57,11 +57,11 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
